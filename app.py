3c01f65b2269e2732e6c71db52e96f1e6977bb0a
tsuruoka <tsuru.dev@gmail.com>
2019-03-23 15:34:36 +0900

    scrape by using google search.

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import urllib
 import pandas as pd
 import numpy as np
 import re
+import requests
+from googlesearch import search
 
 OUTPUT_PATH = './output/local government.csv'
 URL_TOP = 'https://advance21.sakura.ne.jp/chihoujichitai/'
@@ -51,7 +53,7 @@ class SummaryScraper:
             target_url = URL_TOP + pref['url']
 
             try:
-                df = pd.DataFrame(columns=['pref', 'name', 'url'])
+                df = pd.DataFrame(columns=['pref', 'name', 'top_url', 'domain', 'target_url', 'email' ])
                 html = urlopen(target_url)
                 soup = BeautifulSoup(html.read(), "lxml")
                 links = soup.select("center table tr td a")
@@ -61,13 +63,10 @@ class SummaryScraper:
                         continue
                     href = link.get('href')
                     arr = href.split("//")
-                    mail_domain = arr[1]
+                    domain = arr[1]
+                    domain = domain[:-1]
 
-                    if str(link).count('www.'):
-                        mail_domain = mail_domain.replace('www.', '')
-                        mail_domain = mail_domain[:-1]
-
-                    df = df.append({"pref": pref['name'], "name": link.text, "url": href}, ignore_index=True)
+                    df = df.append({"pref": pref['name'], "name": link.text, "top_url": href, 'domain': domain, 'target_url': get_target_url(domain=domain)}, ignore_index=True)
 
                 self.pref_df = pd.concat([self.pref_df, df])
 
@@ -78,21 +77,96 @@ class SummaryScraper:
 
 class LGScraper:
     # 市区町村の中身を扱うクラス.
-    sample_url = 'http://www.city.yokohama.lg.jp/kenko/shogai/yougu/yougu.html'
+    email_list = []
+    emails = []
 
-    def __init__(self):
-        pass
+    def _clear_variable(self):
+        self.email_list = []
+        self.emails = []
 
-    def get_text(self):
+    def get_email(self, url, search_word):
+        self._clear_variable()
         try:
-            html = urlopen(self.sample_url)
+            html = urlopen(url)
             soup = BeautifulSoup(html.read(), "lxml")
-            text = soup.find_all(string=re.compile("@city.yokohama"))
-            print(text)
+            email = soup.find_all(string=re.compile(search_word))
+            self._set_emails(email)
+            if len(self.emails) > 0:
+                print('{}という文字列を発見しました。\nemails: {}'.format(search_word, self.emails))
+            else:
+                print('{}を含む文字列は見つかりませんでした。'.format(search_word))
 
         except Exception as e:
             print(e)
 
+    def _set_emails(self, email):
+        emails = []
+        for e in email:
+            tmp = re.sub(r'\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$', "", e)
+            res = re.sub(tmp, "", e)
+            if self._is_duplicate(res) is False:
+                emails.append(res)
+                self.email_list.append(res)
+        self.emails = emails
+
+    def _is_duplicate(self, email):
+        return email in self.email_list
+
+# Google Search
+def get_target_url(domain):
+    target_url = ''
+    kw = '日常生活用具'
+    query = "{} {}".format(domain, kw)
+    for url in search(query, lang='ja', stop=1):
+        print(url)
+        target_url = url
+    return target_url
+
+# class GoogleSearch:
+    # headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"}
+    # URL_HEAD = 'https://www.google.co.jp/search?num=1&q=site:'
+    # target_url =''
+
+    # def __init__(self):
+        # self.url = '{}{} "{}"'.format(self.URL_HEAD, self.domain, self.KW)
+        # print(self.url)
+        # pass
+
+    # def get_page(self):
+    #     try:
+    #         p = urllib.parse.urlparse(self.url)
+    #         query = urllib.parse.quote_plus(p.query, safe='=&')
+    #         self.url = '{}://{}{}{}{}{}{}{}{}'.format(
+    #             p.scheme, p.netloc, p.path,
+    #             ';' if p.params else '', p.params,
+    #             '?' if p.query else '', query,
+    #             '#' if p.fragment else '', p.fragment
+    #         )
+    #         print(self.url)
+    #         request = urllib.request.Request(self.url, headers=self.headers)
+    #         html = urlopen(request)
+    #         soup = BeautifulSoup(html.read(), "lxml")
+    #         link = soup.select(".r > a")
+    #         title = soup.select(".r > a > .LC20lb")
+    #         if len(link) < 1:
+    #             return
+    #         href = link[0].get('href')
+    #         print(title)
+    #
+    #     except Exception as e:
+    #         print('-----page not found.-----')
+    #         print(e)
+
+
+def gen_search_word(domain):
+    res = ''
+    if str(domain).count('www.'):
+        res = domain.replace('www.', '')
+    res = res.replace('.jp', '')
+    res = res.replace('.lg', '')
+    res = res[:-1]
+    res = "@" + res
+    return res
 
 
 def save_to_csv(df):
@@ -106,15 +180,17 @@ def save_to_csv(df):
 
 
 def execute():
-    # s_scraper = SummaryScraper()
-    # s_scraper.get_prefectures()
-    # s_scraper.pref_list.append({'url': 'hokkaido.html', 'name': '北海道'})
-    # s_scraper.get_cities()
+    s_scraper = SummaryScraper()
+    s_scraper.get_prefectures()
+    s_scraper.get_cities()
     lg_scraper = LGScraper()
-    lg_scraper.get_text()
+    for index, row in s_scraper.pref_d.iterrows():
+        search_word = gen_search_word(domain=row['domain'])
+        lg_scraper.get_email(url=row['target_url'], search_word=search_word)
+        s_scraper.pref_df.at[index, 'email'] = lg_scraper.emails
 
+    save_to_csv(s_scraper.pref_df)
 
-    # save_to_csv(s_scraper.pref_df)
 
 
 if __name__ == '__main__':
